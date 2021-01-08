c8a7b6928bf05564f3c29ecc2c388dd107b535a4
Slava <SlavaShensev@gmail.com>
2021-01-08 18:04:09 +0200

    'done'

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import {MessageType} from "./HW1";
+import a from './Message.module.css';
 
 type ListTypeProps = MessageType
 
 function Message(props: ListTypeProps) {
     return (
-        <div>
+        <div className={a.message}>
             <div>
-                <img src={props.avatar}/>
+                <img className={a.avatar} src={props.avatar}/>
             </div>
-            <div>
-                {props.name}
+            <div className={a.content}>
+                <div className={a.name}>
+                    {props.name}
+                </div>
+                <div className={a.messageTime}>
+                    <div className={a.messageItem}>
+                        {props.message}
+                    </div>
+                    <div className={a.time}>
+                        {props.time}
+                    </div>
+                </div>
             </div>
-            <div>
-                {props.message}
-            </div>
-            <div>
-                {props.time}
-            </div>
+            <div className={a.arrow}></div>
         </div>
     );
 }
